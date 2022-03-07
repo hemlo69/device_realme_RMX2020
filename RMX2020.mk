d93833cc5e075a208f204cfb97b7d3e5a03c8112
Sarthak Roy <sarthakroy2002@gmail.com>
2022-03-08 01:36:45 +0530

    RMX2020: Inherit vendor/extras

--- RMX2020.mk
+++ RMX2020.mk
@@ -18,6 +18,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from vendor/extras
+$(call inherit-product, vendor/extras/extras.mk)
+
+
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
