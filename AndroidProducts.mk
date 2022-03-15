3e532a75082abeb65bba03b099fbee11b9653193
Nitesh <91332849+hemlo69@users.noreply.github.com>
2022-03-15 08:13:28 +0530

    Create AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX2020.mk
+    $(LOCAL_DIR)/aosp_RMX2020.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX2020-user \
-    lineage_RMX2020-userdebug \
-    lineage_RMX2020-eng
+    aosp_RMX2020-user \
+    aosp_RMX2020-userdebug \
+    aosp_RMX2020-eng
