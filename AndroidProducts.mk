5bfcc4bf1789564695ddc5908bfb47983d81f5f2
Nitesh <91332849+hemlo69@users.noreply.github.com>
2022-03-15 23:59:53 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX2020.mk
+    $(LOCAL_DIR)/evolution_RMX2020.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX2020-user \
-    arrow_RMX2020-userdebug \
-    arrow_RMX2020-eng
+    evolution_RMX2020-user \
+    evolution_RMX2020-userdebug \
+    evolution_RMX2020-eng
