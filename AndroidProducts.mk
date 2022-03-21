59b60382df73b5fceb9c7d8d39996d4c6e813a74
Nitesh <91332849+hemlo69@users.noreply.github.com>
2022-03-21 21:33:41 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX2020.mk
+    $(LOCAL_DIR)/palladium_RMX2020.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX2020-user \
-    arrow_RMX2020-userdebug \
-    arrow_RMX2020-eng
+    palladium_RMX2020-user \
+    palladium_RMX2020-userdebug \
+    palladium_RMX2020-eng
