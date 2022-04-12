07551a6c3595dfcacb4cf1b15676316955935c38
Nitesh <91332849+hemlo69@users.noreply.github.com>
2022-04-12 22:49:55 +0530

    Update arrow_RMX2020.mk

--- arrow_RMX2020.mk
+++ arrow_RMX2020.mk
@@ -12,16 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_RMX2020
+PRODUCT_NAME := lineage_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme Monet
 PRODUCT_MANUFACTURER := realme
 
-TARGET_BOOT_ANIMATION_RES := 720
 
 # ArrowOS additions
 DEVICE_MAINTAINER := sarthakroy2002
